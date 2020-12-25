5c2944726515d88999cc21d051596ba0077ccdf0
Rohit Pandey <46323841+rohitpandey8899@users.noreply.github.com>
2020-12-25 23:50:24 +0530

    Add files via upload

---
+++ scritp.js
@@ -0,0 +1,75 @@
+function tally(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br>Basic Accounting<br>Manual Accounting<br>Tally ERP (9,6,5)<br>Tally– Accounting<br>Inventory Management<br>Stock Allocation<br>Advance Accounting<br>Interest Calculation<br>Bill Of Material<br>Introduction to Tally GST GST Theory<br>Cost Centre/ Cost Allocation<br>Data Export/ Import<br>Order Processing<br>TDS/ TCS<br>Payroll<br><br> <b>Job Opportunities<br></b>  Finance<br> MIS Preparation<br> Tally Financial  PlanningPerformance<br> ManagementBudgeting <br> Leadership Development Forecasting<br> MIS Reporting<br> Strategic Planning<br><br> <b>Duration 6 Months		<br>		Fee(Rs 700/- PM)        <br>                      Certificate Charge 250/-</b>";
+}
+function adca(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>- </b><br>Computer Basic<br>MS  Word<br> MS Excel<br> MS PowerPoint<br>DTP Coral Draw<br> Page-Maker & Photoshop<br>Tally ERP (9<br>6<br>5)Manual Accounting<br> Tally ERP (9<br>6<br>5)<br> Tally – Accounting<br> 	Inventory Management<br> Stock Allocation<br> Sale/ Purchase 	Invoice<br> Advance Accounting<br> Interest Calculation<br> Bill Of 	Material<br> Introduction to Tally GST<br> 	GST Theory<br> ITR / 	GST RETURNS<br> Cost Centre/ Cost 	Allocation<br> Data       	Export/ Import<br> Order Processing<br> TDS/ TCS<br> Payroll<br><br> <b>Job Opportunities<br></b>  Finance<br> MIS Preparation<br> Tally Financial  PlanningPerformance<br> ManagementBudgeting <br> Leadership Development Forecasting<br> MIS Reporting<br> Strategic Planning<br> Teacher Training<br><br><b>Duration 1 Years		<br>			Fee (Rs 600/- PM)     <br>Diploma Charge 250/-</b>";
+}
+
+function dm(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br>Market Research<br> Word Press<br> Email Marketing<br> Copywriting<br> SEO (Search Engine Optimization)<br> YouTube Marketing<br> Social Media MarketingLinked in Marketing<br> App Marketing<br> Google Ad words<br> Facebook Ads<br> Google Analytics<br>ORM (online reputation management)<br>Monetization<br><br><b>Duration 60 Days	<br>		Fee. (Rs. 3000/- P.M) </b>	";
+}
+
+function cb(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>- </b><br>Computer Application<br>Computer Basic<br> M.S  Word<br>M.S Excel<br>M.S PowerPoint<br>D.T.PCoral Draw<br> Page-Maker & 	Photoshop<br>Computer Typing<br>	Hindi Computer Typing<br> English Computer Typing.ONLINE PROBLEM SOLUTIONJob Opportunities<br> Data Entry<br> Computer Operating <br> Back Office <br>              MIS executive <br> Administration<br> Front office Executive<br> Teacher Training.<br><br><b>Duration 1 Year 		<br>			Fee. (Rs. 400/- P.M) </b>";
+}
+
+function hd(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br> Computer Fundamentals<br> Study of Components<br> its repair P C Assembling<br> installation<br> Trouble Shooting & Using Software Tools Job Opportunities<br> Hardware EngineerJob Opportunities<br> Network Engineering<br> Hardware Networking<br>          Hardware Maintenance<br> Computer Hardware<br><br><b>bDuration 3 Months	<br> Rs. 1000/- P.M)<br>  Certificate Charge 250/-</b>";
+}
+
+function sp(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-<br>SPECIAL CONTENT</b>	Presentation<br>Basic Lang. CoursesGem session <br>Fluency CoursesPodium (Deal with Ques.) <br>Personality CoursesEnhance Vocabulary 	<br>Voice& Accent ClassesStructured & Unstructured G.D <br>G.D & InterviewDeveloping Soft Skills<br>Debate (Deal with Cross Ques.)<br>Seminar (Speak on Topics)<br>Refine Voice & Accent <br>Interview Skills<br>Creative Classes<br>English Song Classes<br><br><br><b>ONLINE PROBLEM SOLUTION<br><br>Duration 3 Months   <br> Fee. (Rs.1000/- P.M )  </b>";
+}
+
+function ct(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br> English Computer Typing<br>Hindi Computer Typing.Typing Charts<br>Typing Excises<br>Typing Book<br>Typing Magazine<br>Typing Master. <br><br><b> Job Opportunities<br></b> Administration<br> Front Desk<br> Telephone Screening<br>Front Office<br> Computer Operating<br> Back Office Assistant<br> Data  Entry<br><br><b> Duration 6 Months   <br> 	       	        Fee. (Rs. 350/- P.M)     <br>     Certificate Charge 250/-</b>";
+}
+
+function dpt(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br>M.S. Paint<br> Word pad<br>Computer Typing<br> Microsoft Office ToolsMicrosoft Word<br> Microsoft Excel<br> Microsoft PowerPoint<br> Corel Draw<br> Page-Maker<br> Photo Shop<br><br><b>Job Opportunities<br></b> Data Entry<br> Computer Operating <br> Back Office <br>              MIS executive <br> Administration<br> Front office Executive <br><br><b>Duration 6 Months<br>	FEE.(Rs. 350/- P.M) <br>Certificate Charge 250/-</b>";
+}
+
+function ccc(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br>Introduction to Computer Concept<br>Introduction to Operating System<br> WORD PROCESSING<br>SPREAD SHEET<br> PRESENTATION<br>INTRODUCTION TO INTERNET AND WWW<br>E-mail<br> Social Networking and E– Governance Services<br>DIGITAL FINANCIAL TOOLS AND APPLICATIONS<br>OVERVIEW Of FUTURE SKILL & CYBER SECURITY.ONLINE PROBLEM SOLUTION<br><br><b>Duration 3  Months   		<br>	     Fee-(Rs. 1000/-P.M)</b> ";
+}
+
+
+//computer language *******************************************************************************************
+
+
+
+function c(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br> Introduction to Principles of programming<br>Introduction to C Programming<br>Operators and Expressions<br>Data Types and Input/Output Operators<br>Control Statements and Decision Making<br>Arrays and Strings<br>Pointers – I<br>Pointers – II<br>Structures and Unions<br>Functions<br>Storage Classes<br>The Preprocessor<br>File Management<br><br><b>Duration 1  Months   		<br>	     Fee-(Rs. 3000/-P.M)</b> ";
+}
+function cpp(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br> Introduction to Principles of programming<br>Introduction to C Programming<br>Operators and Expressions<br>Data Types and Input/Output Operators<br>Control Statements and Decision Making<br>Arrays and Strings<br>oops<br>Classe<br>Functions<br>File Management<br><br><b>Duration 2  Months   		<br>	     Fee-(Rs. 3000/-P.M)</b> ";
+}
+function java(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br> Introduction to Principles of programming<br>Introduction to C Programming<br>Operators and Expressions<br>Data Types and Input/Output Operators<br>Control Statements and Decision Making<br>Arrays and Strings<br>OOPS (Object Oriented Programming & Systems)<br>Function<br>STRING Manipulation<br>Array<br>Exception Handling<br>I/O Streams<br>Multithreading<br>SQL<br>JDBC<br><br><b>Duration 2  Months   		<br>	     Fee-(Rs. 4000/-P.M)</b> ";
+}
+function python(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br>Introduction to Python and Computer Programming<br>Data Types, Variables, Basic Input-Output Operations, Basic Operators<br>Boolean Values, Conditional Execution, Loops, Lists and List Processing, Logical and Bitwise Operations<br>Functions, Tuples, Dictionaries, and Data Processing<br>Modules, Packages, String and List Methods, and Exceptions<br>The Object-Oriented Approach: Classes, Methods, Objects, and the Standard Objective Features; Exception Handling, and Working with Files<br><br><b>Duration 2  Months   		<br>	     Fee-(Rs. 6000/-P.M)</b> ";
+}
+function android(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br>OOPS (Object Oriented Programming & Systems)<br>Function<br>STRING Manipulation<br>Array<br>Exception Handling<br>I/O Streams<br>Multithreading<br>SQL<br>UI/UX<br>XML<br>Json<br>Activity<br>Services<br>Brodcast<br>Volly<br>Retrofit<br>SQL<br>Notification<br><br><b>Duration 2 - 3  Months   		<br>	     Fee-(Rs. 7000 - 8000 /-P.M)</b> ";
+}
+function react(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br>What is React?<br>Why React?<br>React version history<br>Understanding React event system<br>Understanding Synthetic event<br>Passing arguments to event handlers<br>React 16 vs React 15<br>Just React - Hello World<br>Using create-react-app<br>Anatomy of react project<br>Running the app<br>Debugging first react app<br>Significance of component architecture<br>Types of components - Functional, Class based, Pure<br>Component Composition<br>Using React key prop<br>Using map function to iterate on arrays to generate elements<br><br><b>Duration 1  Months   		<br>	     Fee-(Rs. 2000/-P.M)</b> ";
+}
+function net(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br>Introduction to Networking and the world wide web<br>Building multi-tier enterprise applications.<br>Introduction to the .NET framework.<br>.NET Interoperation services.<br>Client side programming: HTTP, CGI, Cookies, JavaScript, HTML, XML.<br>Server side programming: Web Forms, ASP.NET Web Services, ADO.NET Data Access<br>Client/Server Programming, 3-tier architecture.<br>.NET Remoting.<br>ASP.NET Web services and web service security.<br>RESTful, SOAP, DISCO, and UDDI.<br>Simple Object Access Protocol (SOAP) and Web Services.<br>Software as a Service (SaaS).<br>Cross-Platform Mobile Application Development with HTML5 and PhoneGap<br><br><b>Duration 1  Months   		<br>	     Fee-(Rs. 3000/-P.M)</b> ";
+}
+function js(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br>History of Javascript<br>What is ES6<br>A word on bable<br>Block scope, let & const<br>Template literals<br>Arrow functions<br>Spread and Rest operators<br>Destructuring<br>Classes - Inheritance, Static properties and methods<br>Inheritance<br>Static properties and methods<br>Promise<br>Modules<br><br><b>Duration 1 Months   		<br>	     Fee-(Rs. 2000/-P.M)</b> ";
+}
+function web(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br>Basics of Web DesigningMultimedia and its Applications<br>Web Technologies<br>Introduction to Web Design & Applications<br>Computer Graphics<br>Mathematical Structure for Computer Science<br>HTML<br>CSS<br>JavaScript<br>Bootstrap<br>Adobe Dreamweaver<br>Adobe Flash<br>Available Softwares for Graphic Designing<br>Animation Techniques<br>Best Web Designing Courses Online<br>Web Designing Course Details<br>Basics of Web Designing<br>Web Technologies<br>HTML<br>CSS<br>JavaScript<br>Bootstrap<br>Adobe Dreamweaver<br>Adobe Flash<br>Available Softwares for Graphic Designing<br><br><b>Duration 2  Months   		<br>	     Fee-(Rs. 3000/-P.M)</b> ";
+}
+function sql(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br>Introduction to SQL<br>Introduction to Databases and RDMBS<br>Install a Database Engine<br>SQL Syntax<br>SQL Data Types<br>SQL Operators<br>SQL Expressions<br>SQL Comments<br>SQL – Data Definition Language Commands and Operations.<br>SQL – Data Manipulation Language Commands and Operations<br>SQL – Data Control Language Commands<br>DCL Operations<br>SQL Functions<br>SQL Queries and Sub Queries<br>SQL Clauses<br>SQL Joins<br>SQL Views<br>SQL Indexes<br>SQL Transactions<br>SQL Injection<br>SQL vs. NoSQL<br><br><b>Duration 2  Months   		<br>	     Fee-(Rs. 3000/-P.M)</b> ";
+}
+
+
+function vv(){
+    document.getElementById("demo").innerHTML = "<b>Syllabus<br>-</b><br>";
+}
